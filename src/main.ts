a87365dabb820c4a38f70377ef8f6df7fcf51627
buituananhdev <anhaanh2003@gmail.com>
2024-06-14 23:47:26 +0700

    Test ci/cd

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const PORT = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix('api');
-
+  console.log(process.env.DATABASE_URL);
   await app.listen(PORT, () => console.log(`App is running on port: ${PORT}`));
 }
 
