9cfcc5d3f96f20e945878fb5c5572556b83e4e52
buituananhdev <anhaanh2003@gmail.com>
2024-06-29 12:46:56 +0700

    modify healh check api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode, Header } from '@nestjs/common';
 
 @Controller('health')
 export class AppController {
   @Get()
-  healthCheck(): string {
-    return 'App is running';
+  @HttpCode(200)
+  @Header('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate')
+  @Header('Pragma', 'no-cache')
+  @Header('Expires', '0')
+  healthCheck(): object {
+    return { message: 'App is running' };
   }
 }
