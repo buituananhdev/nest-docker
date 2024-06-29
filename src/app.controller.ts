68d51868a1608a533d3765d3c2fac085c3bfccdd
buituananhdev <anhaanh2003@gmail.com>
2024-06-29 19:07:32 +0700

    add health check in root

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, HttpCode, Header } from '@nestjs/common';
 
-@Controller('health')
+@Controller()
 export class AppController {
   @Get()
   @HttpCode(200)
@@ -8,6 +8,15 @@ export class AppController {
   @Header('Pragma', 'no-cache')
   @Header('Expires', '0')
   healthCheck(): object {
+    return { message: 'App is running' };
+  }
+
+  @Get('health')
+  @HttpCode(200)
+  @Header('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate')
+  @Header('Pragma', 'no-cache')
+  @Header('Expires', '0')
+  healthCheckRoot(): object {
     return { message: 'App is running' };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const PORT = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.setGlobalPrefix('api');
+  // app.setGlobalPrefix('api');
   console.log(process.env.DATABASE_URL);
   await app.listen(PORT, () => console.log(`App is running on port: ${PORT}`));
 }
